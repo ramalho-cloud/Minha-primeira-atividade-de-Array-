aba79589f62d1931956a1aea6c71b41870c014c1
ramalho-cloud <lucas.pressuti@edu.senai.br>
2026-06-16 14:59:14 -0300

    Create Atividade25.js

---
+++ Atividade25.js
@@ -0,0 +1,8 @@
+let tarefas = ["Limpar a casa","ir ao mercado"," tirar o lixo ","arrumar o quarto","alimentar os gatinhos"]
+for (i = 0; i < tarefas.length; i ++){
+    console.log (tarefas[i])
+}
+tarefas.push("alimentar os gatos")
+for (i = 0; i < tarefas.length; i ++){
+    console.log (tarefas[i])
+}
