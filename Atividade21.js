948309c96de923b5db2d749ea5a7043acfbf3ea2
ramalho-cloud <lucas.pressuti@edu.senai.br>
2026-06-16 14:57:44 -0300

    Create Atividade21.js

---
+++ Atividade21.js
@@ -0,0 +1,8 @@
+let nota = [9,6,10,3.4,4]
+for (i = 0; i < nota.length; i++){
+    if (nota[i] >= 6){
+        console.log("nota "+nota[i]+": aprovada")
+    }else {
+        console.log("nota "+nota[i]+": reprovada")
+    }
+}
